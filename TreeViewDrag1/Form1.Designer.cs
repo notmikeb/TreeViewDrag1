c7658324afd3369ab1621e03293cb255a05aba8e
dalongtw chen <dalongtw chen>
2017-03-20 23:44:27 +0800

    init and put files

---
+++ TreeViewDrag1/Form1.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace TreeViewDrag1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.treeView2 = new System.Windows.Forms.TreeView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // treeView1
+            // 
+            this.treeView1.AllowDrop = true;
+            this.treeView1.CheckBoxes = true;
+            this.treeView1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.treeView1.Location = new System.Drawing.Point(0, 0);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(281, 483);
+            this.treeView1.TabIndex = 0;
+            this.treeView1.ItemDrag += new System.Windows.Forms.ItemDragEventHandler(this.treeView1_ItemDrag);
+            this.treeView1.DragDrop += new System.Windows.Forms.DragEventHandler(this.treeView1_DragDrop);
+            this.treeView1.DragEnter += new System.Windows.Forms.DragEventHandler(this.treeView1_DragEnter);
+            // 
+            // treeView2
+            // 
+            this.treeView2.AllowDrop = true;
+            this.treeView2.Dock = System.Windows.Forms.DockStyle.Right;
+            this.treeView2.Location = new System.Drawing.Point(378, 0);
+            this.treeView2.Name = "treeView2";
+            this.treeView2.Size = new System.Drawing.Size(281, 483);
+            this.treeView2.TabIndex = 1;
+            this.treeView2.ItemDrag += new System.Windows.Forms.ItemDragEventHandler(this.treeView2_ItemDrag);
+            this.treeView2.DragDrop += new System.Windows.Forms.DragEventHandler(this.treeView2_DragDrop);
+            this.treeView2.DragEnter += new System.Windows.Forms.DragEventHandler(this.treeView2_DragEnter);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(287, 78);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(287, 126);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(659, 483);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.treeView2);
+            this.Controls.Add(this.treeView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.TreeView treeView2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ TreeViewDrag1/Form1.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TreeViewDrag1
+{
+    public partial class Form1 : Form
+    {
+
+        // one treeview move
+// http://stackoverflow.com/questions/20915260/c-sharp-winforms-dragdrop-within-the-same-treeviewcontrol
+
+        // multiple nodes
+        // https://social.msdn.microsoft.com/Forums/windows/en-US/4b6fb790-5ace-43c3-9a6e-0212f12ece2f/allow-drag-and-drop-of-multiple-nodes-in-tree-view?forum=winforms
+
+        int i = 0;
+        public Form1()
+        {
+            InitializeComponent();
+            button1_Click(this, null);
+            button1_Click(this, null);
+            button1_Click(this, null);
+
+            button2_Click(this, null); button2_Click(this, null);
+            button2_Click(this, null);
+            button2_Click(this, null);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            i++;
+
+            TreeNode newNode = new TreeNode("Text for new node " + i);
+            if (null != treeView1.SelectedNode)
+            {
+                treeView1.SelectedNode.Nodes.Add(newNode);
+                treeView1.SelectedNode.ExpandAll();
+            }
+            else
+            {
+                treeView1.Nodes.Add(newNode);
+            }
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            i++;
+
+            TreeNode newNode = new TreeNode("Number " + i);
+            if (null != treeView2.SelectedNode)
+            {
+                treeView2.SelectedNode.Nodes.Add(newNode);
+                treeView2.SelectedNode.ExpandAll();
+            }
+            else
+            {
+                treeView2.Nodes.Add(newNode);
+
+            }
+            
+        }
+
+        private void treeView1_ItemDrag(object sender, ItemDragEventArgs e)
+        {
+           
+                DoDragDrop(e.Item, DragDropEffects.Move);
+
+        }
+
+        private void treeView1_DragEnter(object sender, DragEventArgs e)
+        {
+            if (sender == treeView1)
+            {
+                e.Effect = DragDropEffects.Move | DragDropEffects.Copy;
+                Console.WriteLine("e.Effect is {0}  allowed is {1}", e.Effect, e.AllowedEffect);
+            }
+
+            if (sender == treeView2)
+            {
+//                e.Effect = DragDropEffects.Copy;
+            }
+        }
+
+        private void treeView1_DragDrop(object sender, DragEventArgs e)
+        {
+
+            // Retrieve the client coordinates of the drop location.
+            TreeView tv = (TreeView)sender;
+            Point targetPoint = tv.PointToClient(new Point(e.X, e.Y));
+
+            // Retrieve the node at the drop location.
+            TreeNode targetNode = tv.GetNodeAt(targetPoint);
+
+            // Retrieve the node that was dragged.
+            TreeNode draggedNode = (TreeNode)e.Data.GetData(typeof(TreeNode));
+
+            TreeNode parentNode = targetNode;
+
+            if (draggedNode != null &&
+                targetNode != null)
+            {
+                bool canDrop = true;
+                while (canDrop && (parentNode != null))
+                {
+                    canDrop = !Object.ReferenceEquals(draggedNode, parentNode);
+                    parentNode = parentNode.Parent;
+                }
+
+                if (canDrop)
+                {
+                    if (e.AllowedEffect == DragDropEffects.Move)
+                    {
+                        draggedNode.Remove();
+                        targetNode.Nodes.Add(draggedNode);
+                    }
+                    else
+                    {
+                        TreeNode newNode = new TreeNode(draggedNode.Text + " Copy");
+                        targetNode.Nodes.Add(newNode);
+                    }
+                    
+                    targetNode.Expand();
+                }
+            }
+        }
+
+        private void treeView2_DragDrop(object sender, DragEventArgs e)
+        {
+            
+        }
+
+        private void treeView2_DragEnter(object sender, DragEventArgs e)
+        {
+            e.Effect = DragDropEffects.Move;
+        }
+
+        private void treeView2_ItemDrag(object sender, ItemDragEventArgs e)
+        {
+            DoDragDrop(e.Item, DragDropEffects.Copy);
+        }
+    }
+}
